612fd7ff672c70f5884381ba612ad663f1c482e8
ArlatPS <przemekx10@gmail.com>
2025-03-10 18:13:01 +0100

    nx with sam plugin

---
+++ NotificationsService-e2e/jest.config.ts
@@ -0,0 +1,22 @@
+export default {
+  displayName: 'NotificationsService-e2e',
+  preset: '../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../coverage/NotificationsService-e2e',
+  collectCoverageFrom: ['src/**/*.ts', '!jest.config.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  coverageReporters: ['lcov', 'text'],
+  resetMocks: true,
+  testTimeout: 10000,
+};

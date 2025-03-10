6203985e62a1788aacdd82e42ea87701d9fca1e4
ArlatPS <przemekx10@gmail.com>
2025-03-10 20:24:52 +0100

    deploy tested

--- vendor-service/src/handlers/get-all-items.mjs
+++ vendor-service/src/handlers/get-all-items.mjs
@@ -17,37 +17,29 @@ export const getAllItemsHandler = async (event) => {
         throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
     }
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('HELLO')
-    };
-
-    return response;
-
     // All log statements are written to CloudWatch
     console.info('received:', event);
 
-//     // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
-//     // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
-//     // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
-//     var params = {
-//         TableName : tableName
-//     };
-//
-//     try {
-//         const data = await ddbDocClient.send(new ScanCommand(params));
-//         var items = data.Items;
-//     } catch (err) {
-//         console.log("Error", err);
-//     }
-//
-//     const response = {
-//         statusCode: 200,
-//         body: JSON.stringify(items)
-//     };
-//
-//     // All log statements are written to CloudWatch
-//     console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
-//     return response;
-// }
+    // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
+    // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
+    var params = {
+        TableName : tableName
+    };
+
+    try {
+        const data = await ddbDocClient.send(new ScanCommand(params));
+        var items = data.Items;
+    } catch (err) {
+        console.log("Error", err);
+    }
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(items)
+    };
+
+    // All log statements are written to CloudWatch
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
 }

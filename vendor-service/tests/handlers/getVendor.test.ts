4bd00f997f60f0164f49e347d5047512c50e0d7e
ArlatPS <przemekx10@gmail.com>
2025-03-13 13:45:39 +0100

    cleanup

--- vendor-service/tests/handlers/getVendor.test.ts
+++ vendor-service/tests/handlers/getVendor.test.ts
@@ -3,6 +3,8 @@ import { handler } from '../../src/handlers/getVendor';
 import { expect, describe, it } from '@jest/globals';
 
 describe('Unit test for app handler', function () {
+    process.env.MESSAGE = 'AWS';
+
     it('verifies successful response', async () => {
         const event: APIGatewayProxyEvent = {
             httpMethod: 'get',
@@ -58,7 +60,7 @@ describe('Unit test for app handler', function () {
         expect(result.statusCode).toEqual(200);
         expect(result.body).toEqual(
             JSON.stringify({
-                message: 'hello world',
+                message: 'hello world AWS',
             }),
         );
     });

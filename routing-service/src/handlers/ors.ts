e9eade31511591969e3e702eeb511f9323eea9cc
ArlatPS <przemekx10@gmail.com>
2025-04-23 21:25:27 +0200

    createDeliveryJobs

--- routing-service/src/handlers/ors.ts
+++ routing-service/src/handlers/ors.ts
@@ -2,66 +2,35 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import axios from 'axios';
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    console.log('initiated');
     try {
-        const response = await axios.post(
-            'https://api.openrouteservice.org/optimization',
-            {
-                jobs: [
-                    {
-                        id: 1,
-                        service: 60,
-                        delivery: [1],
-                        location: [1.98465, 48.70329],
-                        skills: [1],
-                        time_windows: [[32400, 36000]],
-                    },
-                    { id: 2, service: 60, delivery: [1], location: [2.03655, 48.61128], skills: [1] },
-                    { id: 3, service: 60, delivery: [1], location: [2.39719, 49.07611], skills: [2] },
-                    { id: 4, service: 60, delivery: [1], location: [2.41808, 49.22619], skills: [2] },
-                    { id: 5, service: 60, delivery: [1], location: [2.28325, 48.5958], skills: [14] },
-                    { id: 6, service: 60, delivery: [1], location: [2.89857, 48.90736], skills: [14] },
-                    { id: 7, service: 60, delivery: [1], location: [2.41608, 49.22619], skills: [2] },
-                    { id: 8, service: 60, delivery: [1], location: [2.28525, 48.5958], skills: [14] },
-                    { id: 9, service: 60, delivery: [1], location: [2.89457, 48.90736], skills: [14] },
-                    { id: 10, service: 60, delivery: [1], location: [2.46808, 49.22619], skills: [2] },
-                    { id: 11, service: 60, delivery: [1], location: [2.23325, 48.5958], skills: [14] },
-                    { id: 12, service: 60, delivery: [1], location: [2.89357, 48.90736], skills: [14] },
-                ],
-                vehicles: [
-                    {
-                        id: 1,
-                        profile: 'driving-car',
-                        start: [2.35044, 48.71764],
-                        end: [2.35044, 48.71764],
-                        capacity: [4],
-                        skills: [1, 14],
-                        time_window: [28800, 43200],
-                    },
-                    {
-                        id: 2,
-                        profile: 'driving-car',
-                        start: [2.35044, 48.71764],
-                        end: [2.35044, 48.71764],
-                        capacity: [4],
-                        skills: [2, 14],
-                        time_window: [28800, 43200],
-                    },
-                ],
-            },
-            {
-                headers: {
-                    Accept: 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
-                    Authorization: process.env.ORS_API_KEY,
-                    'Content-Type': 'application/json; charset=utf-8',
-                },
-                timeout: 10000,
-            },
-        );
-
         return {
             statusCode: 200,
-            body: JSON.stringify(response.data),
+            body: JSON.stringify(
+                await createDeliveryJobs(
+                    [
+                        { id: 1, capacity: 20 },
+                        { id: 2, capacity: 20 },
+                        { id: 3, capacity: 20 },
+                    ],
+                    {
+                        id: 1,
+                        location: {
+                            longitude: 19.928709,
+                            latitude: 50.050259,
+                        },
+                        timeWindow: [0, 5000],
+                    },
+                    Array.from({ length: 50 }, (_, i) => {
+                        return {
+                            id: i,
+                            location: {
+                                longitude: Number((19.928709 + Math.random() * 0.1).toFixed(5)),
+                                latitude: Number((50.050259 + Math.random() * 0.1).toFixed(5)),
+                            },
+                        };
+                    }),
+                ),
+            ),
         };
     } catch (error) {
         console.error('Error:', error);
@@ -70,8 +39,110 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
             statusCode: error.response?.status || 500,
             body: JSON.stringify({
                 message: 'Failed to process the request',
-                error: error.message,
+                error,
             }),
         };
     }
 };
+
+interface Vehicle {
+    id: string;
+    capacity: number;
+}
+
+interface Warehouse {
+    id: string;
+    location: Location;
+    timeWindow: [number, number];
+}
+
+interface Delivery {
+    id: string;
+    location: Location;
+}
+
+interface Location {
+    longitude: number;
+    latitude: number;
+}
+
+interface Step {
+    type: 'start' | 'job' | 'end';
+    location: Location;
+    arrivalTime: number;
+    deliveryId?: string;
+}
+
+interface DeliveryJob {
+    steps: Step[];
+    vehicleId: string;
+    duration: number;
+}
+
+interface RouteFromOrs {
+    vehicle: string;
+    duration: number;
+    steps: {
+        type: 'start' | 'job' | 'end';
+        location: [number, number];
+        arrival: number;
+        id?: string;
+    }[];
+}
+
+async function createDeliveryJobs(
+    vehicles: Vehicle[],
+    warehouse: Warehouse,
+    deliveries: Delivery[],
+): Promise<DeliveryJob[]> {
+    const response = await axios.post<{ routes: RouteFromOrs[] }>(
+        'https://api.openrouteservice.org/optimization',
+        {
+            jobs: deliveries.map((delivery: Delivery) => ({
+                id: delivery.id,
+                service: 60,
+                delivery: [1],
+                skills: [1],
+                location: [delivery.location.longitude, delivery.location.latitude],
+                time_windows: [warehouse.timeWindow],
+            })),
+            vehicles: vehicles.map((vehicle: Vehicle) => ({
+                id: vehicle.id,
+                profile: 'driving-car',
+                skills: [1],
+                start: [warehouse.location.longitude, warehouse.location.latitude],
+                end: [warehouse.location.longitude, warehouse.location.latitude],
+                capacity: [vehicle.capacity],
+                time_window: warehouse.timeWindow,
+            })),
+        },
+        {
+            headers: {
+                Accept: 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
+                Authorization: process.env.ORS_API_KEY,
+                'Content-Type': 'application/json; charset=utf-8',
+            },
+            timeout: 10000,
+        },
+    );
+
+    const optimizedRoutes = response.data.routes;
+
+    return optimizedRoutes.map((route: RouteFromOrs) => {
+        const steps = route.steps.map((step) => ({
+            type: step.type,
+            location: {
+                longitude: step.location[0],
+                latitude: step.location[1],
+            },
+            arrivalTime: step.arrival,
+            deliveryId: step.id,
+        }));
+
+        return {
+            vehicleId: route.vehicle,
+            duration: route.duration,
+            steps,
+        };
+    });
+}

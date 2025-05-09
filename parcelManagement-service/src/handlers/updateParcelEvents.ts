53754552c2d348afd12b51f616d8fbc2cf2d31b3
ArlatPS <przemekx10@gmail.com>
2025-05-09 13:20:37 +0200

    updateParcelEvents behind a queue

--- parcelManagement-service/src/handlers/updateParcelEvents.ts
+++ parcelManagement-service/src/handlers/updateParcelEvents.ts
@@ -1,4 +1,4 @@
-import { Context } from 'aws-lambda';
+import { Context, SQSEvent } from 'aws-lambda';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
@@ -7,19 +7,24 @@ import { Parcel, ParcelEvent } from '../aggregates/parcel';
 const client = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocumentClient.from(client);
 
-export const handler = async (event: ParcelEvent, context: Context): Promise<void> => {
-    if (!event.detail.data.parcelId) {
-        throw new Error(`Invalid event data ${JSON.stringify(event)}`);
-    }
+export const handler = async (event: SQSEvent, context: Context): Promise<void> => {
+    for (const record of event.Records) {
+        let parcelEvent: ParcelEvent;
 
-    const parcel = new Parcel(ddbDocClient, context);
+        try {
+            parcelEvent = JSON.parse(record.body) as ParcelEvent;
 
-    try {
-        await parcel.loadState(event.detail.data.parcelId);
+            if (!parcelEvent.detail.data.parcelId) {
+                throw new Error(`Invalid event data ${JSON.stringify(parcelEvent)}`);
+            }
 
-        await parcel.saveEvent(event);
-    } catch (err) {
-        console.error('Failed to update parcel state:', err);
-        throw new Error(`Failed to update parcel state: ${err}`);
+            const parcel = new Parcel(ddbDocClient, context);
+
+            await parcel.loadState(parcelEvent.detail.data.parcelId);
+            await parcel.saveEvent(parcelEvent);
+        } catch (err) {
+            console.error('Failed to process SQS message:', err);
+            throw new Error(`Failed to process SQS message: ${err}`);
+        }
     }
 };
